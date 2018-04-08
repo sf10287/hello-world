7a9b3be57a36dd43ba93c6d8e72252fea95fdcf6
FENG SHAN <sf10287@163.com>
2018-04-08 15:07:49 +0800

    changes for a.c

--- a.c
+++ a.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include "b.h"
 
 int main()
 {
+    int val_a = 10;
+    int val_b = 20;
+
     printf("Hello world!\n");
+    printf("a + b = %d\n", add(val_a, val_b));
 
     return 0;
 }
