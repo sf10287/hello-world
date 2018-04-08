0a562702b98c5f2ae6bc4b2eab88140cf9c82c3f
FENG SHAN <sf10287@163.com>
2018-04-08 15:04:00 +0800

    add get cpu num

--- info.c
+++ info.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void get_page_info(unsigned int *page_size)
+void get_page_info (unsigned int *page_size)
 {
     *page_size = sysconf(_SC_PAGE_SIZE);
+}
+
+void get_cpu_num (unsigned int *cpu_num)
+{
+    *cpu_num = sysconf(_SC_NPROCESSORS_ONLN);
 }
